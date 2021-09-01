65a7ad1b5e8fec2b2cb82fc9bd24a1bf117c6e49
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-09-01 10:15:42 +0530

    ADDED: Player can see the Board and  can Choose valid cells

---
+++ com/bl/workshop/TicTacToe.java
@@ -0,0 +1,41 @@
+package com.bl.workshop;
+/**
+ * As a Player would like to see the board so I can choose the
+ *                     valid cells to make my move during my turn
+ * Write a method showBoard to display the current Board
+ */
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        char[] board = new char[10];
+
+        System.out.println("Game initiated!");
+
+        //Display the initial board
+        for(int i = 0; i < board.length; i++) {
+            board[i] = '-';
+        }
+        showboard(board);
+
+        char inputChoice = chooseLetter();
+        System.out.println("Player has chosen " +inputChoice);
+
+    }
+
+    public static void showboard(char[] board) {
+        System.out.println("\n" + "Displaying the Tic Tac Toe Board");
+        System.out.println(board[0] + "  " + board[1] + "  " + board[2]);
+        System.out.println(board[3] + "  " + board[4] + "  " + board[5]);
+        System.out.println(board[6] + "  " + board[7] + "  " + board[8]);
+    }
+
+    public static char chooseLetter() {
+        System.out.println("\n" + "Enter your choice: X | O");
+        char choice = input.next().charAt(0);
+
+        return choice;
+    }
+}
